01b9cb79e04f34454442437b8176ec9ca0c97bad
Malli123456 <mallikarjuna.aws@gmail.com>
2018-05-12 13:37:25 +0530

    Added a ne if condition

--- MyWebProject/src/com/test/project/User.java
+++ MyWebProject/src/com/test/project/User.java
@@ -3,15 +3,20 @@ package com.test.project;
 public class User {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("User Class");
 		System.out.println("Commited the changes");
 
 		System.out.println("Commited the changes");
 		System.out.println("Hello Janardhan");
-System.out.println("Hello Malliiiiiiii");
+		System.out.println("Hello Malliiiiiiii");
 
-		
+		boolean value = true;
+
+		if (value) {
+			System.out.println("Checking the If Condition..");
+		}
+
 	}
 
 }
